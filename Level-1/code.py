2fb137bf899de367eeb4e1db927cee9bc154545d
linux <lenio.passos@gmail.com>
2023-06-18 20:45:52 +0100

    Level-1 solved

--- Level-1/code.py
+++ Level-1/code.py
@@ -1,7 +1,7 @@
 '''
 ////////////////////////////////////////////////////////////
 ///                                                      ///
-///   0. tests.py is passing but the code is vulnerable  /// 
+///   0. tests.py is passing but the code is vulnerable  ///
 ///   1. Review the code. Can you spot the bug?          ///
 ///   2. Fix the code but ensure that tests.py passes    ///
 ///   3. Run hack.py and if passing then CONGRATS!       ///
@@ -12,22 +12,29 @@
 '''
 
 from collections import namedtuple
+from decimal import Decimal
+import math
 
 Order = namedtuple('Order', 'id, items')
 Item = namedtuple('Item', 'type, description, amount, quantity')
 
 def validorder(order: Order):
-    net = 0
-    
+    net = Decimal.from_float(0)
+
     for item in order.items:
+
         if item.type == 'payment':
-            net += item.amount
+            net += Decimal.from_float(item.amount)
+            #print("SOMA NET: " + str(net))
         elif item.type == 'product':
-            net -= item.amount * item.quantity
+            net -= Decimal.from_float(item.amount) * Decimal.from_float(item.quantity)
+            #print("SUBTRAI NET: " + str(net) + "e " + str(item.amount))
         else:
             return("Invalid item type: %s" % item.type)
-    
-    if net != 0:
+
+    #if net != 0:
+    #print("NET: FIM " + str(net))
+    if math.isclose(net,0, abs_tol=1e-9)==False:
         return("Order ID: %s - Payment imbalance: $%0.2f" % (order.id, net))
     else:
         return("Order ID: %s - Full payment received!" % order.id)
